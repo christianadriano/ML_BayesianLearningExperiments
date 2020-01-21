53481acdc326aa18e481cc19eaba9e7c84bd482e
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-01-22 00:37:10 +0100

    MCMC to approximate the posterior

--- probabilityYesAnswers_BayesianUpdate.R
+++ probabilityYesAnswers_BayesianUpdate.R
@@ -122,6 +122,32 @@ probabilities given the different p. We express this equation in R language as f
 
 R <- likelihood(h,n,p_prime)/likelihood(h,n,p) * (dbeta(p_prime,1,1)/dbeta(p,1,1))  
 
+# Set the lenght of the loop (Marcov Chain, number of iterations).  
+nrep <- 5000  
+# Start the loop (MCMC)  
+for (i in 1:nrep) {  
+  # Obtain a new proposal value for p  
+  p_prime <- p + runif(1, -0.05,0.05)  
+  # Avoid values out of the range 0 - 1  
+  if (p_prime < 0) {p_prime <- abs(p_prime)}  
+  if (p_prime > 1) {p_prime <- 2 - p_prime}  
+  # Compute the acceptance proability using our likelihood function and the  
+  # beta(1,1) distribution as our prior probability.  
+  R <- likelihood(h,n,p_prime)/likelihood(h,n,p) * (dbeta(p_prime,1,1)/dbeta(p,1,1))  
+  # Accept or reject the new value of p  
+  if (R > 1) {R <- 1}  
+  random <- runif (1,0,1)  
+  if (random < R) {  
+    p <- p_prime  
+  }  
+  # Store the likelihood of the accepted p and its value  
+  posterior[i,1] <- log(likelihood(h, n, p))  
+  posterior[i,2] <- p  
+  print(i)  
+}  
+
+
+
 ###############################
 
 x <- rbeta(n=500, shape1=2, shape2=2)
